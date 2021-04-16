5c9d25260cb7b8052101c15107ca749d9919291c
anlaganlag <anlaganlag@gmail.com>
2021-04-16 15:31:51 +0800

    字符串处理完毕

--- 416.py
+++ 416.py
@@ -1,5 +1,143 @@
+from selenium.webdriver.chrome
+from selenium.webdriver.chrome.options import Options  # 无界面
+from selenium.common.exceptions import TimeoutException
+from concurrent.futures import ThreadPoolExecutor
+from selenium import webdriver
+from fateadm_api import TestFunc
+import datetime
+import requests
 import pymssql
+import logging
+import time
+import re
+import os
+
 import pprint
+
+
+class MyViewSpider():
+    def __init__(self):
+        self.headers = {
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+        }
+        logging.basicConfig(level=logging.WARNING,  # 控制台打印的日志级别
+                    filename=f'{datetime.datetime.now().date()}new.log',
+                    filemode='a',  ##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                    # a是追加模式，默认如果不写的话，就是追加模式
+                    format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+        self.pattern_goodsStar = re.compile(r'\d+.\d+')
+        self.pattern = re.compile(r'\d+')
+        self.d = self.webdriverBasic()
+        
+        def webdriverBasic(self):
+            chrome_options = Options()
+            chrome_options.add_argument(
+                '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"')  # 请求头
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片
+            chrome_options.add_argument('lang=en-US.UTF-8')  # 修复美国站出现中文的问题
+            chrome_options.add_argument('--headless')  # 隐藏界面
+            chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+            chrome_options.add_argument(
+                "user-data-dir=C:\\Users\\Administrator\\AppData\Local\\Google\\Chrome\\User Data1")  # 使用这个 条件必定要隐藏界面 否则报错
+            d = webdriver.Chrome(options=chrome_options)  # 生成无界面对象
+            d.set_page_load_timeout(20)  # 设置最长等待时间
+            d.maximize_window()
+            return d
+        def made_task(self, taskList):
+            for task in taskList:
+                item = {}
+                item['taskid'] = task['taskid']
+                item['taskLink'] = task['taskurl']  # 索引
+                item["site"] = self.GetSite(item['taskLink'])
+                self.Scheduling_task(item)
+
+        def Scheduling_task(self, item, i=0):
+            try:
+                while True:
+                    try:
+                        self.d.get(item['taskLink'])
+                        break
+                    except TimeoutException:
+                        print(f'页面超时:{item["taskLink"]}')
+                        logging.warning(f'页面超时:{item["taskLink"]}')
+
+                item['taskStartTime'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                page_count = self.d.page_source
+                if len(page_count) >= 10000:
+                    self.change_Postcode(self.d, item)  # 更换邮编
+                    self.get_data(self.d, item, i)
+                else:
+                    res = self.handle_Verification_Code(self.d, item, i)
+                    if res == 0:  # 验证码页加载出错
+                        self.Scheduling_task(item, i)
+
+            except Exception as e:
+                print(f'{e},地址:{item["taskLink"]}')
+                logging.error(f'{e},错误所在行数{e.__traceback__.tb_lineno} --地址:{item["taskLink"]}')  # 列表超出索引  有可能是更换邮编时间过长导致
+            
+        def get_data(self, d, item, i=0):
+            DataList = []
+            goodsList = d.find_elements_by_xpath('.//div[@data-component-type="s-search-result"]')
+            for goods in goodsList:
+                ad = goods.find_elements_by_xpath('.//span[@class="a-size-mini a-color-secondary"]')  # 广告     是否考虑放在全局变量
+                if not ad:
+                    item_goods = {}
+                    i += 1
+                    try:
+                        Link = goods.find_element_by_xpath('.//a[@class="a-link-normal a-text-normal"]').get_attribute(
+                            'href')
+                    except:
+                        continue
+                    try:
+                        Title = goods.find_elements_by_xpath('.//a[@class="a-link-normal a-text-normal"]/span')[0].text
+                    except:
+                        Title = ""
+                    try:
+                        Star = goods.find_element_by_xpath('.//div[@class="a-row a-size-small"]/span').get_attribute(
+                            'aria-label')
+                        GoodsStar = self.pattern_goodsStar.findall(Star)[0]
+                    except:
+                        GoodsStar = ""
+                    try:
+                        CommentNum = goods.find_elements_by_xpath('.//span[@class="a-size-base"]')[0].text.replace(',', "")
+                    except:
+                        CommentNum = ""
+                    try:
+                        Price = goods.find_elements_by_xpath('.//a[@class="a-size-base a-link-normal a-text-normal"]/span')[
+                            0].text.replace('\n', ".")
+                    except:
+                        Price = ""
+
+                    item_goods['Ranking'] = str(i)
+                    Asin = Link.split("/")[5]
+                    if len(Asin) == 10:
+                        item_goods["goodsAsin"] = Asin
+                    else:
+                        item_goods["goodsAsin"] = Link.split("/")[4]
+                        if len(item_goods["goodsAsin"]) != 10:  # 如果以这种方式还抓不到ASIN则 这条数据不要
+                            continue
+
+                    item_goods['Title'] = Title.replace("'", "''")
+                    if item["site"] != "JP":
+                        item_goods['Brand'] = item_goods['Title'].split(" ")[0]
+                    else:
+                        item_goods['Brand'] = ""
+                    item_goods['Star'] = GoodsStar
+                    item_goods['CommentNum'] = CommentNum.replace(".", "")
+                    item_goods['Price'] = Price
+                    item_goods['goodsLink'] = Link
+                    print(item_goods)
+                    DataList.append(item_goods)
+                else:
+                    continue
+
+            self.SaveAtDataDb(DataList, item, i)  # 第一时间入库
+
+
+
+
+
 class HandleTask():
     def ReadDBTask(self):
         connect = pymssql.connect('192.168.2.163', 'sa', 'JcEbms123', 'EBMS')  # 服务器名,账户,密码,数据库名
---
+++ ParaseHtml.py
@@ -0,0 +1,460 @@
+s = """<img src="https://www.amazon.ca/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"/>gordy4.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    When it arrived I put it on and fit like it was made for it
+  
+Reviewed in Canada on April 9, 2016Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  Actually bought this case for an RCA Pro Edition 10.Measured everything and ordered it.When it arrived I put it on and fit like it was made for it.Nice case for the price paid.
+
+  
+Read more
+  One person found this helpful
+      
+          Helpful
+      
+  
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"/>Russ Chester3.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    Not as good of quality as others I have purchased for $50 ...
+  
+Reviewed in Canada on May 20, 2016Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  Hard to buy many things sight unseen. Not as good of quality as others I have purchased for $50 but again it was much cheaper and you get what you pay for.
+
+  
+Read more
+  
+      
+          Helpful
+      
+  
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/0906ea72-f404-47be-b0d3-5749dd1a6ef9._CR0,0,500,500_SX48_.jpg"/>Nicole Laverdure5.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    Excellent!
+  
+Reviewed in Canada on November 17, 2015Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  I love it, I didn't know what to expect! What a nice surprise, it's a very good quality price! Excellent for my tablet!
+
+  
+Read more
+  
+      
+          Helpful
+      
+  
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"/>Amazon Customer4.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    Arrived promptly and exactly as pictured
+  
+Reviewed in Canada on April 18, 2016Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  Item arrived quickly and looked exactly as shown.  It didn't fit my tablet, but that is my own fault for not measuring the tablet and just assuming the size.
+
+  
+Read more
+  One person found this helpful
+      
+          Helpful
+      
+  
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"/>Dene C.5.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    Outstanding in every respect
+  
+Reviewed in Canada on June 2, 2016Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  perfect product, absolutely the best value, delivered the following day. outstanding
+
+  
+Read more
+  
+      
+          Helpful
+      
+  
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"/>nightale5.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    Five Stars
+  
+Reviewed in Canada on November 17, 2016Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  Excellent value. I've had it for over 3 months and still looks and feels great. I love the handle.
+
+  
+Read more
+  
+      
+          Helpful
+      
+  
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"/>Amazon Customer3.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    Three Stars
+  
+Reviewed in Canada on October 22, 2015Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  It looks fine, but hard to stand because you have to pull it's bottom side  out from the backboard.
+
+  
+Read more
+  
+      
+          Helpful
+      
+  
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"/>Albert G Cavallaro5.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    Perfect Case for Samsung Galaxy Tab 10.1
+  
+Reviewed in the United States on January 15, 2012Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  I spent quite a bit of time shopping for the Samsung Galaxy Tab 10.1 case that meets all of my needs. These include having multi-angle viewing capability, a thin and attractive profile, being able to charge the tablet when the case is completely closed, and a reasonable price. This case is a winner in every aspect. Beware that many cases on the market do not have an opening which will allow you to charge the tablet in the closed position. This one does which differentiates it from the competition. The case fits perfectly leaving openings for all buttons, the speakers, headphone jack, camera lenses, and charging port. The quality of the leather surface is very good as well. I also utilize all of the multiple viewing angle choices that are available depending on how I am sitting with the tablet. This is the perfect case at the right price and you won't go wrong by purchasing it!
+
+  
+Read more
+  502 people found this helpful
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/139a2250-d701-4259-8815-424c3792af4f._CR0,0.0,333,333_SX48_.jpg"/>Josh Hagen3.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    Item isn't well made
+  
+Reviewed in the United States on February 25, 2013Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  The item is somewhat sleek and does have a nice feel to it, but that is about where it ends. As far as the 3 slots that your tablet set in so you can view it at an angle, they are too shallow. The smallest bump or adjustment will cause it to either slide down a row, or completely come off.  The clip the cover uses to snap onto the tablet also is off center. All in all, it's ok, but I wish I would have bought a different item.
+
+  
+Read more
+  
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/46be990b-5bb7-4b4b-8c54-44165e6d6935._CR90,0,540,540_SX48_.jpg"/>Mama4.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    Good enough
+  
+Reviewed in the United States on October 24, 2015Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  I like this cover, but it falls off all the time. My toddler figured out how to get it off in a matter of minutes. It has protected it against falls though. He stand is a nice feature and there's 3 different places to put it so it's always at a good angle.
+
+  
+Read more
+  
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/6d70105c-ce15-4be9-a974-9fc4dd5444c8._CR0,0,500,500_SX48_.jpg"/>Patty_Mick5.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    Great case
+  
+Reviewed in the United States on June 7, 2014Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  Great case.. Simple.. great price.. stands up for watching videos.. and is very well protected. if you don't want a keyboard and your just looking to protect your tablet this is a great choice.
+
+  
+Read more
+  
+        Report abuse
+  
+<img src="https://www.amazon.ca/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"/>K. Albright4.0 out of 5 stars
+
+
+
+
+
+
+
+  
+  
+    great for the price
+  
+Reviewed in the United States on June 11, 2013Verified Purchase
+
+
+
+
+
+
+
+
+
+  
+  
+    
+  For the price this is a really nice case.... the only issue I have is the hook like closure doesnt hook (maybe it's not supposed to?). Either way, I like that it can be used as a stand and would buy this again.
+
+  
+Read more
+  
+        Report abuse
+  """
+
+
+def get_data(d):
+        DataList = []
+        # xpathAllReviewXpath = 'div[@data-hook="review"]'
+        # xpathReviewCurCountryXpath =".//div[@class='a-section review aok-relative']"
+        # reviews = d.find_elements_by_xpath(xpathReviewCurCountryXpath)
+        s1=[i for  i in d.split("Report abuse") if len(i.strip())>10] #每个评论以Report abuse 分段，且去掉最后的空评论
+
+
+        # p1,title,info,comment,READMORE,helpfulNum,_=[i.strip() for i in str.splitlines() if len(i.replace(" ", ""))>0]
+        for review in s1:
+            # print("==========================================start================================")
+            reviewItem = {}
+            # l=[i.strip() for i in str.splitlines(review) if len(i.replace(" ", ""))>0]
+            p1,p2,p3,p4,*p5 = [i.strip() for i in str.splitlines(review) if len(i.strip())>0 ]
+
+            reviewItem["p1"] = p1
+            
+
+            reviewItem["title"] = p2
+            reviewItem["shoppinginfo"] = p3
+
+            reviewItem["comment"] = p4
+
+            if len(p5)>2:
+              reviewItem["helpNum"] = p5[1]
+            else:
+              reviewItem["helpNum"] = 0
+        
+
+            # if len(review)>=7:
+            #     print("====================",helpfulNum)
+
+            #     reviewItem["helpfulNum"] = helpfulNum[0]
+            # else:
+            #     reviewItem["helpfulNum"] = 0
+            DataList.append(reviewItem)
+        return DataList
+data = get_data(s)
+print(data)
+
+
+
+            
+
+
+            
