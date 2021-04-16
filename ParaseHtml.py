791355a5cc906f2855687314ad0a515f8219fa48
anlaganlag <anlaganlag@gmail.com>
2021-04-16 16:47:28 +0800

    完成了正则匹配和name与star的分配

--- ParaseHtml.py
+++ ParaseHtml.py
@@ -411,44 +411,51 @@ Read more
         Report abuse
   """
 
-
+import re
 def get_data(d):
-        DataList = []
-        # xpathAllReviewXpath = 'div[@data-hook="review"]'
-        # xpathReviewCurCountryXpath =".//div[@class='a-section review aok-relative']"
-        # reviews = d.find_elements_by_xpath(xpathReviewCurCountryXpath)
-        s1=[i for  i in d.split("Report abuse") if len(i.strip())>10] #每个评论以Report abuse 分段，且去掉最后的空评论
+
+  DataList = []
+  matchObj = re.compile(r'\/\>(.*)(\d+\.\d)+')
+  manNstar = matchObj.findall(d)
+  # print(manNstar)
 
 
-        # p1,title,info,comment,READMORE,helpfulNum,_=[i.strip() for i in str.splitlines() if len(i.replace(" ", ""))>0]
-        for review in s1:
-            # print("==========================================start================================")
-            reviewItem = {}
-            # l=[i.strip() for i in str.splitlines(review) if len(i.replace(" ", ""))>0]
-            p1,p2,p3,p4,*p5 = [i.strip() for i in str.splitlines(review) if len(i.strip())>0 ]
+  # xpathAllReviewXpath = 'div[@data-hook="review"]'
+  # xpathReviewCurCountryXpath =".//div[@class='a-section review aok-relative']"
+  # reviews = d.find_elements_by_xpath(xpathReviewCurCountryXpath)
+  s1=[i for  i in d.split("Report abuse") if len(i.strip())>10] #每个评论以Report abuse 分段，且去掉最后的空评论
 
-            reviewItem["p1"] = p1
-            
 
-            reviewItem["title"] = p2
-            reviewItem["shoppinginfo"] = p3
+  # p1,title,info,comment,READMORE,helpfulNum,_=[i.strip() for i in str.splitds() if len(i.replace(" ", ""))>0]
+  for idx,review in enumerate(s1):
+      # print("==========================================start================================")
+      reviewItem = {}
+      # l=[i.strip() for i in str.splitlines(review) if len(i.replace(" ", ""))>0]
+      _,p2,p3,p4,*p5 = [i.strip() for i in str.splitlines(review) if len(i.strip())>0 ]
 
-            reviewItem["comment"] = p4
+      reviewItem["name"] = manNstar[idx][0]
+      reviewItem["star"] = manNstar[idx][1]
+      
 
-            if len(p5)>2:
-              reviewItem["helpNum"] = p5[1]
-            else:
-              reviewItem["helpNum"] = 0
-        
+      reviewItem["title"] = p2
+      reviewItem["shoppinginfo"] = p3
 
-            # if len(review)>=7:
-            #     print("====================",helpfulNum)
+      reviewItem["comment"] = p4
 
-            #     reviewItem["helpfulNum"] = helpfulNum[0]
-            # else:
-            #     reviewItem["helpfulNum"] = 0
-            DataList.append(reviewItem)
-        return DataList
+      if len(p5)>2:
+        reviewItem["helpNum"] = p5[1]
+      else:
+        reviewItem["helpNum"] = 0
+  
+
+      # if len(review)>=7:
+      #     print("====================",helpfulNum)
+
+      #     reviewItem["helpfulNum"] = helpfulNum[0]
+      # else:
+      #     reviewItem["helpfulNum"] = 0
+      DataList.append(reviewItem)
+  return DataList
 data = get_data(s)
 print(data)
 
