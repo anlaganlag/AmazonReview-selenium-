243cc26a0a6f6a14c39b9f32c4fb8d4a4b312bfa
anlaganlag <anlaganlag@gmail.com>
2021-04-22 11:54:03 +0800

    取出单独的html

--- req.py
+++ req.py
@@ -3,53 +3,79 @@ from lxml import etree
 
 def cleanse(s):
     return s.replace('\\','')
-URL = 'https://www.amazon.ca/dp/B00B0O1BBW'
-URL2 = 'https://www.amazon.com/hz/reviews-render/ajax/medley-filtered-reviews/get/ref=cm_cr_dp_d_fltrs_srt'
-URL3 = ' https://www.amazon.com/hz/reviews-render/ajax/medley-filtered-reviews/get/ref=cm_cr_dp_d_fltrs_srt'
-body = {
-    "language": "en_US",
-    # "asin": "B07QQZDHMR",
-    "asin": "B00B0O1BBW",
-    "sortBy": "helpful",
-    "scope": "reviewsAjax2",
-}
-headers = {
+
+
+
+def Scheduling_task(item,url):
+    headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-        }
-res = requests.post(URL3,headers=headers,data=body)
-resText = cleanse(res.text)
+    }
+    # URL = 'https://www.amazon.ca/dp/B00B0O1BBW'
+    # URL2 = 'https://www.amazon.com/hz/reviews-render/ajax/medley-filtered-reviews/get/ref=cm_cr_dp_d_fltrs_srt'
+    # URL3 = ' https://www.amazon.com/hz/reviews-render/ajax/medley-filtered-reviews/get/ref=cm_cr_dp_d_fltrs_srt'
+    headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+            }
+    body = {
+        "asin": f"{item['ASIN']}",
+        "sortBy": "helpful",
+        "scope": "reviewsAjax2",
+    }
+    r = requests.post(url,headers=headers,data=body)
+    r.encoding='utf-8'
+    json_text=r.text
+    # resText = cleanse(res.text)
+    myLIST=json_text.split("&&&")
+    for msg in myLIST[3:-5]:
+        itemObj=msg.replace("\n","")
+        html=list(eval(itemObj))[2]
+        html_x = etree.HTML(html)
+        with open(f"{list(eval(itemObj))[2][9:15]}.txt","w") as f:
+            f.write(etree.tostring(html_x).decode())
+        try:
+            #获取用户名、
+            pr_name=html_x.xpath("//span[@class='a-profile-name']/text()")[0]
+            #获取评分
+            pf= html_x.xpath("//span[@class='a-icon-alt']/text()")[0].split(" ")[0]
+            print("    ",pr_name,"给出的评分是",pf)
+        except Exception as inst:
+            print(type(inst))    # the exception instance
+            print(inst.args)     # arguments stored in .args
+            print(inst)
+url = 'https://www.amazon.com/hz/reviews-render/ajax/medley-filtered-reviews/get/ref=cm_cr_dp_d_fltrs_srt'
+item={"ASIN":"B00AAVEWA8"}
+
+Scheduling_task(item,url)
 
 
+    # with open("test0422.txt","w") as f:
+    #     f.write(etree.tostring(html_index).decode())
 
-html_index = etree.HTML(resText)
-with open("test0422.txt","w") as f:
-    f.write(etree.tostring(html_index).decode())
+    # userName = html_index.xpath('//span[@class="a-profile-name"]/text()') #用户名
+    # <a class="a-link-normal" title="4.0 out of 5 stars" 
+    # stars = html_index.xpath('//a[@class="a-link-normal"]/@title')
+    # title = html_index.xpath('//a[@data-hook="review-title"]/span/text()')
+    # reviewDate = html_index.xpath('//span[@data-hook="review-date"]/text()')
+    # <span data-hook=\"helpful-vote-statement\" class=\"a-size-base a-color-tertiary cr-vote-text\">4 people found this helpful</span>
+    # helpful = html_index.xpath('//span[@data-hook="helpful-vote-statement"]/text()')
+    # reviewContent = html_index.xpath('//div[@class="a-expander-content reviewText review-text-content a-expander-partial-collapse-content"]/span/text()')
+    # reviewImage= html_index.xpath('//img[@data-hook="review-image-tile"]/@data-src')
 
-userName = html_index.xpath('//span[@class="a-profile-name"]/text()') #用户名
-# <a class="a-link-normal" title="4.0 out of 5 stars" 
-stars = html_index.xpath('//a[@class="a-link-normal"]/@title')
-title = html_index.xpath('//a[@data-hook="review-title"]/span/text()')
-reviewDate = html_index.xpath('//span[@data-hook="review-date"]/text()')
-# <span data-hook=\"helpful-vote-statement\" class=\"a-size-base a-color-tertiary cr-vote-text\">4 people found this helpful</span>
-helpful = html_index.xpath('//span[@data-hook="helpful-vote-statement"]/text()')
-reviewContent = html_index.xpath('//div[@class="a-expander-content reviewText review-text-content a-expander-partial-collapse-content"]/span/text()')
-reviewImage= html_index.xpath('//img[@data-hook="review-image-tile"]/@data-src')
-
-# print(userName)
-print(stars)
-# print(title)
-# print(reviewDate)
-print(helpful)
-# print(reviewContent)
-print(reviewImage)
-# print(etree.tostring(html_index).decode())
-# title = titleEle.get_attribute("title")
-# print(titleEle)
-# print(res.text)
-# print(res.url)
+    # print(userName)
+    # print(stars)
+    # print(title)
+    # print(reviewDate)
+    # print(helpful)
+    # print(reviewContent)
+    # print(reviewImage)
+    # print(etree.tostring(html_index).decode())
+    # title = titleEle.get_attribute("title")
+    # print(titleEle)
+    # print(res.text)
+    # print(res.url)
 
 
-#总结下怎么抽revierw的数据
-#首先是通过review切换recent top找出url...
-    # 确定了url后就是post获取数据
-    # 需要提供了data等于body...
+    #总结下怎么抽revierw的数据
+    #首先是通过review切换recent top找出url...
+        # 确定了url后就是post获取数据
+        # 需要提供了data等于body...
--- req2.py
+++ req2.py
@@ -20,17 +20,60 @@ r = requests.post(URL3,headers=headers,data=body)
 
 r.encoding='utf-8'
 json_text=r.text
+with open("req2.txt","w") as f:
+    f.write(json_text)
 myLIST=json_text.split("&&&")
 for msg in myLIST[2:-5]:
     item=msg.replace("\n","")
-    # 得到节点
-    html=list(eval(item))[2]
+    html=eval(item)[2]
     html_x = etree.HTML(html)
     try:
         # 获得评分
         pf= html_x.xpath("//span[@class='a-icon-alt']/text()")[0].split(" ")[0]
         # 评论者的用户名
         pr_name=html_x.xpath("//span[@class='a-profile-name']/text()")[0]
-        print("    ",pr_name,"给出的评分是",pf)
+        # print("    ",pr_name,"给出的评分是",pf)
     except:
         pass
+
+
+# def api_fr(asin): #orderBY[recent,helpful]
+#     for msg in [['recent',"reviewsAjax3"],['helpful',"reviewsAjax2"]]:
+#         if msg[0]=='recent':
+#             status='most 评分'
+#         else:
+#             status='top 评分'
+#         print("当前模式是:%s"%status)
+#         headers_fr = {
+#             "Accept": "text/html,*/*",
+#             "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+#             "Origin": "https://www.amazon.fr",
+#             "Referer": "https://www.amazon.fr/dp/%s?th=1"%asin,
+#             "Sec-Fetch-Mode": "cors",
+#             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
+#             "X-Requested-With": "XMLHttpRequest"
+#         }
+#         data={
+#             "asin": asin,
+#             "sortBy": msg[0],       #helpful&reviewsAjax2
+#             "scope": msg[1]
+#         }
+#         r=requests.post(url_fr,data=data,headers=headers_fr)
+#         r.encoding='utf-8'
+#         json_text=r.text
+#         myLIST=json_text.split("&&&")
+#         for msg in myLIST[2:-5]:
+#         #2是标题标题 -5是最后一条非评论，由于有最后的&&&分割
+              #干掉换行符...
+#             item=msg.replace("\n","")
+#             # 得到节点
+#             html=list(eval(item))[2]
+#             html_x = etree.HTML(html)
+#             try:
+#                 # 获得评分
+#                 pf= html_x.xpath("//span[@class='a-icon-alt']/text()")[0].split(" ")[0]
+#                 # 评论者的用户名
+#                 pr_name=html_x.xpath("//span[@class='a-profile-name']/text()")[0]
+#                 print("    ",pr_name,"给出的评分是",pf)
+#             except:
+#                 pass
